7782244d3ab55cad02c9750b3d627f3f379cd1a6
Nathan Mora <104747322+nathanmorax@users.noreply.github.com>
2026-09-16 20:54:10 -0600

    Initial commit

---
+++ Riff/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Riff
+//
+//  Created by Satori Tech 341 on 16/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Riff/RiffApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiffApp.swift
+//  Riff
+//
+//  Created by Satori Tech 341 on 16/09/26.
+//
+
+import SwiftUI
+
+@main
+struct RiffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
